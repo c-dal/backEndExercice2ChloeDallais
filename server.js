82a5b9d58192561a6ca1944569ee043176d8f046
c-dal <chlo04.dls@gmail.com>
2025-10-16 09:56:37 +0200

    Add code for server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotevn from 'dotenv';
+import taskRoutes from './routes/taskRoutes.js'; 
+
+dotevn.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/todos", taskRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
